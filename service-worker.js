2a7c0f6c43ae37850ec12d9b546323c51bed9382
Auoris <harryedmondsonuk@gmail.com>
2025-08-23 14:31:11 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "chatfic-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  // add any other local files you reference (icons/, images/, sounds/)
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+  ));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
