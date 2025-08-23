7d8264e5b449e0fae2fa73c5ee9e8c70e9253992
Auoris <harryedmondsonuk@gmail.com>
2025-08-23 15:00:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,31 @@
+// bump this string when you change cached files
 const CACHE = "chatfic-v1";
+
+// keep paths relative so it works on GitHub Pages project URLs
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  // add any other local files you reference (icons/, images/, sounds/)
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/maskable-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
-  ));
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
   );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  }
 });
